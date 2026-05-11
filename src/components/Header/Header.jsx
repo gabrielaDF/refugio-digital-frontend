845b9ec09c8994cfdb595da99e672ae7ccc44ea6
gabrielaDF <defreitasgabi@gmail.com>
2026-05-10 19:34:54 -0500

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,11 @@ function Header() {
   return (
     <header className="header">
       <div className="logo-container">
-        <img src="/logo-huella.png" alt="Logo Refugio Digital" className="logo" />
+        <img
+          src="https://res.cloudinary.com/dn6wibef0/image/upload/v1761692506/refugio%20digital/logo_refugio_digital_ytkc5v.png"
+          alt=" Logo Refugio Digital"
+          className="logo"
+        />
         <h1 className="titulo">Refugio Digital</h1>
       </div>
 
@@ -23,10 +27,18 @@ function Header() {
 
       {/* Navegación */}
       <nav className={`nav ${menuAbierto ? "activo" : ""}`}>
-        <Link to="/" onClick={() => setMenuAbierto(false)}>Inicio</Link>
-        <Link to="/adopciones" onClick={() => setMenuAbierto(false)}>Adopciones</Link>
-        <Link to="/reportes" onClick={() => setMenuAbierto(false)}>Reportes</Link>
-        <Link to="/historias" onClick={() => setMenuAbierto(false)}>Historias</Link>
+        <Link to="/" onClick={() => setMenuAbierto(false)}>
+          Inicio
+        </Link>
+        <Link to="/adopciones" onClick={() => setMenuAbierto(false)}>
+          Adopciones
+        </Link>
+        <Link to="/reportes" onClick={() => setMenuAbierto(false)}>
+          Reportes
+        </Link>
+        <Link to="/historias" onClick={() => setMenuAbierto(false)}>
+          Historias
+        </Link>
       </nav>
     </header>
   );
