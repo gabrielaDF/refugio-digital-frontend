dfdcf970202eb0e754e58aa8736e623eb06936fd
MichelleGonzalez2007 <michellegonzalezg2007@gmail.com>
2026-05-16 18:25:42 -0500

    ajustes de css, home y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
+
 import Home from "./pages/Home";
+//import Adopciones from "./pages/Adopciones";
+//import Reportes from "./pages/Reportes";
+//import Historias from "./pages/Historias";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* <Route path="/adopciones" element={<Adopciones />} />
+
+        <Route path="/reportes" element={<Reportes />} />
+
+        <Route path="/historias" element={<Historias />} /> */}
+      </Routes>
     </>
   );
 }
