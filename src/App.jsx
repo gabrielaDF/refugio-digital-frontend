2c8266be3bba94fa08d063e48bf14dd122f09a55
gabrielaDF <defreitasgabi@gmail.com>
2026-08-29 23:01:12 -0500

    actualización

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-
 import Header from "./components/Header/Header";
-
 import Footer from "./components/Footer/Footer";
-
 import Home from "./pages/Home";
 import Adopciones from "./pages/Adopciones";
 import Reportes from "./pages/Reportes";
